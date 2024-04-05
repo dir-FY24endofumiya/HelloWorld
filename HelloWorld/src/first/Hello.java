b76717f5720351575fa7727afff59ac83e0352c4
dir-FY24endofumiya <fumiya.endo.l024@dirbato.co.jp>
2024-04-05 11:49:33 +0900

    Gitの練習です

--- HelloWorld/src/first/Hello.java
+++ HelloWorld/src/first/Hello.java
@@ -3,8 +3,8 @@ package first;
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");        // TODO 自動生成されたメソッド・スタブ
-
+        System.out.println("Hello, World!");
+        System.out.println("こんにちは、世界！");// TODO 自動生成されたメソッド・スタブ
     }
 
 }
